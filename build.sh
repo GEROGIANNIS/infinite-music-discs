c61a5f031f57cbce4f41e0c6e53a7c7244ed1865
Lefteris Garyfalakis <lefterisgar@gmail.com>
2022-05-30 20:32:56 +0300

    Add build script for Linux

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pyinstaller main.pyw --onefile --clean --noconfirm \
+    --version-file "version.rc" \
+    --add-data "data/*:data" \
+    --name "imd-gui" \
+    --icon "data/jukebox_256.ico" \
+    --distpath bin \
